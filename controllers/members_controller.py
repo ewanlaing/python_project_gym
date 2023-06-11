8a45b5dc970e61bde16cdbed5e2898691e35e4fc
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-06-11 11:11:18 +0100

    members side of html seems functional

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -11,6 +11,7 @@ members_blueprint = Blueprint("members", __name__)
 @members_blueprint.route("/members/index", methods=['GET'])
 def home():
     members = member_repository.select_all()
+    members.sort(key=lambda x: x.name)
     return render_template("/members/index.html", members=members)
 
 @members_blueprint.route("/members/add", methods=['GET'])
@@ -27,4 +28,30 @@ def create_member():
     active = True
     member = Member(name, premium, active)
     member_repository.save(member)
+    return redirect('/members/index')
+
+@members_blueprint.route("/members/<id>/edit", methods=['GET'])
+def edit(id):
+    member = member_repository.select(id)
+    return render_template("/members/edit.html", member=member)
+
+@members_blueprint.route("/members/<id>", methods=['POST'])
+def update_member(id):
+    name = request.form['name']
+    if request.form.get('standard'):
+        premium = True
+    if request.form.get('premium'):
+        premium = False
+    if request.form.get('deactivate'):
+        active = False
+    if request.form.get('activate'):
+        active = True
+    active = True
+    member = Member(name, premium, active, id)
+    member_repository.update(member)
+    return redirect('/members/index')
+
+@members_blueprint.route("/members/<id>/delete", methods=['POST'])
+def delete_member(id):
+    member_repository.delete(id)
     return redirect('/members/index')
